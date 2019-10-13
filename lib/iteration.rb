b2e1eadf68f639918c331cf21a9c1e6069cf87be
ZUBEYDE SISMAN <zubeydesisman@gmail.com>
2019-10-13 19:37:05 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -38,7 +38,7 @@ def total_even_pairs(src)
   new_array = []
   i = 0 
   while i < src.length do 
-    if src[i][0]%2 == 0 && src[i][0] == 0 
+    if src[i][0] %2 == 0 && src[i][0]  %2== 0 
       new_array = src[i][0] + src[i][0]
     end 
      i += 1 
